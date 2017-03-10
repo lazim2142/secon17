f320e43c36bea2ac97f872ad8184eaf4f4b52f86
Muhaimen Shamsi <lazim2142@hotmail.com>
2017-03-10 16:22:00 -0500

    Work in progress on servo control

--- secon17.ino
+++ secon17.ino
@@ -4,12 +4,12 @@
 
 IRSensor ir_front_short(IR1);
 IRSensor ir_front_long(IR2);
-IRSensor ir_left_short(IR7);
-IRSensor ir_left_long(IR8);
 IRSensor ir_right_short(IR3);
 IRSensor ir_right_long(IR4);
-IRSensor ir_back_short(IR5);
-IRSensor ir_back_long(IR6);
+IRSensor ir_back_short(IR6);
+IRSensor ir_back_long(IR5);
+IRSensor ir_left_short(IR7);
+IRSensor ir_left_long(IR8);
 
 float x, y;
 int orientation = 0;
@@ -23,11 +23,21 @@ void setup ()
 
 void loop()
 {
-  IRSensor::updateAllSensors();
-  Localization::updatePose();
-  Motion::goToPosition(0, 0);
+  //IRSensor::updateAllSensors();
+  //Serial.println(ir_back_long.getInches());
+  //IRSensor::printSensors();
+  
+  if(millis() > 3000)
+  {
+    Motion::disableMotors();
+    return;
+  }
+  
+  Motion::setMotorSpeed(1, 50000);
+  //Motion::setMotorSpeed(2, 1300);
+  //Motion::setMotorSpeed(3, 1000);
+  //Motion::setMotorSpeed(4, 1000);
+  
+  //Localization::updatePose();  
+  //Motion::goToPosition(0, 0);
 }
-
-
-
-
